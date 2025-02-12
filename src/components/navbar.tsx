852d3581a76247604cf2db609ac8ca3199961318
Rainnny7 <braydonrainnny@gmail.com>
2025-02-11 22:17:57 -0500

    hide the punishment categories in the nav if youre not authorized

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,13 +1,13 @@
-import { env } from "~/env";
-import Image from "next/image";
-import type { ReactElement } from "react";
-import Link from "next/link";
-import { UserButton } from "@clerk/nextjs";
-import { getAllPunishmentCategories } from "~/types/punishment-category";
-import { Badge } from "~/components/ui/badge";
-import { db } from "~/server/drizzle";
+import { Protect, SignedIn, UserButton } from "@clerk/nextjs";
 import { count } from "drizzle-orm";
+import Image from "next/image";
+import Link from "next/link";
+import type { ReactElement } from "react";
+import { Badge } from "~/components/ui/badge";
+import { env } from "~/env";
 import { numberWithCommas } from "~/lib/utils";
+import { db } from "~/server/drizzle";
+import { getAllPunishmentCategories } from "~/types/punishment-category";
 
 const Navbar = (): ReactElement => (
     <nav className="-mx-7 px-7 py-5 flex justify-between items-center border-b border-muted">
@@ -31,30 +31,32 @@ const Navbar = (): ReactElement => (
             </Link>
 
             {/* Categories */}
-            <div className="flex gap-3 items-center">
-                {getAllPunishmentCategories().map(async (category) => {
-                    const recordCountResult = await db
-                        .select({ count: count() })
-                        .from(category.table);
-                    return (
-                        <Link
-                            key={category.type}
-                            className="px-2.5 py-1 flex gap-2 items-center bg-muted/40 text-sm rounded-lg hover:opacity-75 transition-all transform-gpu"
-                            href={`/records/${category.type}`}
-                        >
-                            <span>{category.displayName}s</span>
-                            <Badge
-                                className="hidden lg:flex px-2 border-white/20"
-                                variant="outline"
+            <Protect>
+                <div className="flex gap-3 items-center">
+                    {getAllPunishmentCategories().map(async (category) => {
+                        const recordCountResult = await db
+                            .select({ count: count() })
+                            .from(category.table);
+                        return (
+                            <Link
+                                key={category.type}
+                                className="px-2.5 py-1 flex gap-2 items-center bg-muted/40 text-sm rounded-lg hover:opacity-75 transition-all transform-gpu"
+                                href={`/records/${category.type}`}
                             >
-                                {numberWithCommas(
-                                    recordCountResult.at(0)?.count ?? 0
-                                )}
-                            </Badge>
-                        </Link>
-                    );
-                })}
-            </div>
+                                <span>{category.displayName}s</span>
+                                <Badge
+                                    className="hidden lg:flex px-2 border-white/20"
+                                    variant="outline"
+                                >
+                                    {numberWithCommas(
+                                        recordCountResult.at(0)?.count ?? 0
+                                    )}
+                                </Badge>
+                            </Link>
+                        );
+                    })}
+                </div>
+            </Protect>
         </div>
 
         {/* Right */}
