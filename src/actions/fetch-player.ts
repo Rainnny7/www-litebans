631d9252a9a7f785c9e9c7d8e03aa69abcf7a6ec
Rainnny7 <braydonrainnny@gmail.com>
2025-02-21 18:12:36 -0500

    fix searching not working with usernames

--- src/actions/fetch-player.ts
+++ src/actions/fetch-player.ts
@@ -10,15 +10,25 @@ import { historyRecords } from "~/common/drizzle/schema";
 import { isBedrockUuid, STEVE_AVATAR } from "~/common/player";
 import { type TablePlayerData } from "~/types/punishment-record";
 
+/**
+ * An in-memory cache for player data.
+ */
 const playerCache = new AppCache({
     ttl: 1000 * 60 * 60 * 24, // 24 hours
     checkInterval: 1000 * 60 * 60, // 1 hour
 });
 
+/**
+ * Fetch player data from the database with the given query.
+ *
+ * @param query the query to fetch data for, uuid or username
+ * @returns the player data if found, undefined otherwise
+ */
 export const fetchPlayerData = async (
-    uuid: string
+    query: string
 ): Promise<TablePlayerData | undefined> => {
-    if (uuid === "CONSOLE") return undefined;
+    const isUuid: boolean = query.length === 36;
+    if (query === "CONSOLE") return undefined;
 
     // Check if the user is authorized
     const { userId } = await auth();
@@ -26,26 +36,32 @@ export const fetchPlayerData = async (
         forbidden();
     }
 
-    return await fetchWithCache(playerCache, `player:${uuid}`, async () => {
+    return await fetchWithCache(playerCache, `player:${query}`, async () => {
         try {
             const before = performance.now();
             const player: any = (
                 await db
                     .select()
                     .from(historyRecords)
-                    .where(eq(historyRecords.uuid, uuid))
+                    .where(
+                        eq(
+                            isUuid ? historyRecords.uuid : historyRecords.name,
+                            query
+                        )
+                    )
                     .limit(1)
             )[0];
 
             console.log(
-                `[API::fetchPlayerData] Took ${performance.now() - before}ms for ${uuid}`
+                `[API::fetchPlayerData] Took ${performance.now() - before}ms for ${query}`
             );
             return {
-                uuid,
+                uuid: player.uuid,
                 username: player.name,
-                avatar: isBedrockUuid(uuid)
-                    ? STEVE_AVATAR
-                    : `https://api.restfulmc.cc/player/head/${uuid}.png`,
+                avatar:
+                    isUuid && isBedrockUuid(query)
+                        ? STEVE_AVATAR
+                        : `https://api.restfulmc.cc/player/head/${query}.png`,
             };
         } catch (err) {
             return undefined;
